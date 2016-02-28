b0ea52828d4dfe163dcc62e2b21beffd80277cb2
FlyRoNik <hang.glider.viru007@gmail.com>
2016-02-28 13:50:15 +0200

    Выделил координаты в переменную, при 25 съедает кек, предусмотрел без исходную сетуацтю без перезапуска!

--- src/main/java/com/codenjoy/dojo/snake/client/YourSolver.java
+++ src/main/java/com/codenjoy/dojo/snake/client/YourSolver.java
@@ -32,137 +32,140 @@ public class YourSolver implements Solver<Board> {
         Point point_app = board.getApples().get(0);
         Point point_snake = board.getHead();
         Point point_app_kek = board.getStones().get(0);
-
         List<Point> point_snake_body = board.getSnake();
-
-        int x = point_snake.getX();
-        int y = point_snake.getY();
-
-        int t = point_snake_body.size();
-        if (t > 10) {
-            x = point_snake_body.get(1).getX();
-            y = point_snake_body.get(1).getY();
-        }
 
         String result = "";
         char key = 0;
 
-        if (point_snake.getX() > point_app.getX()) {
+        int x_head = point_snake.getX();
+        int y_head = point_snake.getY();
+        int x_app = point_app.getX();
+        int y_app = point_app.getY();
+        char good_apple = Elements.GOOD_APPLE.ch();
+
+        if (point_snake_body.size() == 25) {
+            x_app = point_app_kek.getX();
+            y_app = point_app_kek.getY();
+            good_apple = Elements.BAD_APPLE.ch();
+        }
+
+        if (x_head > x_app) {
             key = 'l';
-            if (board.getField()[point_snake.getX()-1][point_snake.getY()] == Elements.NONE.ch() ||
-                    board.getField()[point_snake.getX()-1][point_snake.getY()] == Elements.GOOD_APPLE.ch()) {
+            if (board.getField()[x_head -1][y_head] == Elements.NONE.ch() ||
+                    board.getField()[x_head -1][y_head] == good_apple) {
                 result = Direction.LEFT.toString();
             }
         }
 
-        if (point_snake.getX() < point_app.getX()){
+        if (x_head < x_app){
             key = 'r';
-            if (board.getField()[point_snake.getX()+1][point_snake.getY()] == Elements.GOOD_APPLE.ch() ||
-                    board.getField()[point_snake.getX()+1][point_snake.getY()] == Elements.NONE.ch()) {
+            if (board.getField()[x_head +1][y_head] == good_apple ||
+                    board.getField()[x_head +1][y_head] == Elements.NONE.ch()) {
                 result =  Direction.RIGHT.toString();
             }
         }
 
-        if (point_snake.getY() > point_app.getY()) {
+        if (y_head > y_app) {
             key = 'u';
-            if (board.getField()[point_snake.getX()][point_snake.getY()-1] == Elements.NONE.ch()||
-                    board.getField()[point_snake.getX()][point_snake.getY()-1] == Elements.GOOD_APPLE.ch()) {
+            if (board.getField()[x_head][y_head -1] == Elements.NONE.ch()||
+                    board.getField()[x_head][y_head -1] == good_apple) {
                 result = Direction.UP.toString();
             }
         }
 
-        if (point_snake.getY() < point_app.getY()) {
+        if (y_head < y_app) {
             key = 'd';
-            if (board.getField()[point_snake.getX()][point_snake.getY()+1] == Elements.NONE.ch()||
-                    board.getField()[point_snake.getX()][point_snake.getY()+1] == Elements.GOOD_APPLE.ch()) {
+            if (board.getField()[x_head][y_head +1] == Elements.NONE.ch()||
+                    board.getField()[x_head][y_head +1] == good_apple) {
                 result = Direction.DOWN.toString();
             }
         }
 
-//        if (point_app_kek.getX() == point_snake.getX() &&
-//                !(Math.abs(point_app.getY()-point_snake.getY())<Math.abs(point_app.getY()-point_app_kek.getY())) &&
-//                (board.getField()[point_snake.getX()][point_snake.getY()] == Elements.HEAD_UP.ch() ||
-//                        board.getField()[point_snake.getX()][point_snake.getY()] == Elements.HEAD_DOWN.ch())) {
-//            if (board.getField()[point_snake.getX() + 1][point_snake.getY()] != Elements.BREAK.ch() && point_snake.getX() < point_app.getX()) {
-//                result = Direction.RIGHT.toString();
-//            } else {
-//                result = Direction.LEFT.toString();
-//            }
-//        }
-//
-//        if (point_app_kek.getY() == point_snake.getY() &&
-//                !(Math.abs(point_app.getX()-point_snake.getX())<Math.abs(point_app.getX()-point_app_kek.getX())) &&
-//                (board.getField()[point_snake.getX()][point_snake.getY()] == Elements.HEAD_RIGHT.ch() ||
-//                        board.getField()[point_snake.getX()][point_snake.getY()] == Elements.HEAD_LEFT.ch())) {
-//
-//            if (board.getField()[point_snake.getX()][point_snake.getY() - 1] != Elements.BREAK.ch() && point_snake.getY() > point_app.getY()) {
-//                result = Direction.UP.toString();
-//            } else {
-//                result = Direction.DOWN.toString();
-//            }
-//        }
-
         if (result == "") {
             switch (key) {
                 case 'l':{
-                    if (board.getField()[point_snake.getX()][point_snake.getY()-1] == Elements.NONE.ch()||
-                            board.getField()[point_snake.getX()][point_snake.getY()-1] == Elements.GOOD_APPLE.ch()) {
+                    if (board.getField()[x_head][y_head -1] == Elements.NONE.ch()||
+                            board.getField()[x_head][y_head -1] == good_apple) {
                         result = Direction.UP.toString();
                     }else {
-                        if (board.getField()[point_snake.getX()][point_snake.getY() + 1] == Elements.NONE.ch() ||
-                                board.getField()[point_snake.getX()][point_snake.getY() + 1] == Elements.GOOD_APPLE.ch()) {
+                        if (board.getField()[x_head][y_head + 1] == Elements.NONE.ch() ||
+                                board.getField()[x_head][y_head + 1] == good_apple) {
                             result = Direction.DOWN.toString();
                         } else {
-                            result = Direction.RIGHT.toString();
+                            if (board.getField()[x_head +1][y_head] == good_apple ||
+                                    board.getField()[x_head +1][y_head] == Elements.NONE.ch()) {
+                                result =  Direction.RIGHT.toString();
+                            }else {
+                                //do nothing
+                            }
                         }
                     }
                     break;
                 }
 
                 case 'r':{
-                    if (board.getField()[point_snake.getX()][point_snake.getY()-1] == Elements.NONE.ch()||
-                            board.getField()[point_snake.getX()][point_snake.getY()-1] == Elements.GOOD_APPLE.ch()) {
+                    if (board.getField()[x_head][y_head -1] == Elements.NONE.ch()||
+                            board.getField()[x_head][y_head -1] == good_apple) {
                         result = Direction.UP.toString();
                     }else {
-                        if (board.getField()[point_snake.getX()][point_snake.getY() + 1] == Elements.NONE.ch() ||
-                                board.getField()[point_snake.getX()][point_snake.getY() + 1] == Elements.GOOD_APPLE.ch()) {
+                        if (board.getField()[x_head][y_head + 1] == Elements.NONE.ch() ||
+                                board.getField()[x_head][y_head + 1] == good_apple) {
                             result = Direction.DOWN.toString();
                         } else {
-                            result = Direction.LEFT.toString();
+                            if (board.getField()[x_head -1][y_head] == Elements.NONE.ch() ||
+                                    board.getField()[x_head -1][y_head] == good_apple) {
+                                result = Direction.LEFT.toString();
+                            }else {
+                                //do nothing
+                            }
                         }
                     }
                     break;
                 }
 
                 case 'u':{
-                    if (board.getField()[point_snake.getX()-1][point_snake.getY()] == Elements.NONE.ch() ||
-                            board.getField()[point_snake.getX()-1][point_snake.getY()] == Elements.GOOD_APPLE.ch()) {
+                    if (board.getField()[x_head -1][y_head] == Elements.NONE.ch() ||
+                            board.getField()[x_head -1][y_head] == good_apple) {
                         result = Direction.LEFT.toString();
                     }else {
-                        if (board.getField()[point_snake.getX() + 1][point_snake.getY()] == Elements.GOOD_APPLE.ch() ||
-                                board.getField()[point_snake.getX() + 1][point_snake.getY()] == Elements.NONE.ch()) {
+                        if (board.getField()[x_head + 1][y_head] == good_apple ||
+                                board.getField()[x_head + 1][y_head] == Elements.NONE.ch()) {
                             result = Direction.RIGHT.toString();
                         } else {
-                            result = Direction.DOWN.toString();
+                            if (board.getField()[x_head][y_head + 1] == Elements.NONE.ch() ||
+                                    board.getField()[x_head][y_head + 1] == good_apple) {
+                                result = Direction.DOWN.toString();
+                            } else {
+                                //do nothing
+                            }
                         }
                     }
                     break;
                 }
                 case  'd':{
-                    if (board.getField()[point_snake.getX()-1][point_snake.getY()] == Elements.NONE.ch() ||
-                            board.getField()[point_snake.getX()-1][point_snake.getY()] == Elements.GOOD_APPLE.ch()) {
+                    if (board.getField()[x_head -1][y_head] == Elements.NONE.ch() ||
+                            board.getField()[x_head -1][y_head] == good_apple) {
                         result = Direction.LEFT.toString();
                     }else {
-                        if (board.getField()[point_snake.getX() + 1][point_snake.getY()] == Elements.GOOD_APPLE.ch() ||
-                                board.getField()[point_snake.getX() + 1][point_snake.getY()] == Elements.NONE.ch()) {
+                        if (board.getField()[x_head + 1][y_head] == good_apple ||
+                                board.getField()[x_head + 1][y_head] == Elements.NONE.ch()) {
                             result = Direction.RIGHT.toString();
                         } else {
-                            result = Direction.UP.toString();
+                            if (board.getField()[x_head][y_head - 1] == Elements.NONE.ch() ||
+                                    board.getField()[x_head][y_head - 1] == good_apple) {
+                                result = Direction.UP.toString();
+                            } else {
+                                //do nothing
+                            }
                         }
                     }
                     break;
                 }
             }
+        }
+
+        if (result == "") {
+            result = Direction.UP.toString();
         }
 
         return result;
